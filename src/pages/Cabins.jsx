ab0b2dde28771bbab3761a2adf9a6de0cc520b3d
Cxff <adidaodididoudou@163.com>
2024-06-24 19:17:18 +0800

    feat: canbinForm 增加cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import Button from "../ui/Button.jsx";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -11,6 +16,12 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+
+        <Button size="medium" onClick={() => setShowForm((show) => !show)}>
+          Add new canbin
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
