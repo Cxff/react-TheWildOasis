c24212cca3e30d48d4580a6c54a6f8a704d01502
Cxff <adidaodididoudou@163.com>
2024-07-09 16:36:12 +0800

    feat: cabins完成增加筛选

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,26 +1,23 @@
 import styled, { css } from "styled-components";
 
-const test = css`
-`
+const test = css``;
 
 const Heading = styled.h1`
-  ${
-          (props) => {
-            props.as === 'h1' && css`
-              font-size: 3rem;
-              font-weight: 600;
-            `
-          }
-  }
-  ${
-          (props) => {
-            props.as === 'h2' && css`
-              font-size: 2rem;
-              font-weight: 600;
-            `
-          }
-  }
-  ${ test }
-`
+  ${(props) => {
+    props.as === "h1" &&
+      css`
+        font-size: 3rem;
+        font-weight: 600;
+      `;
+  }}
+  ${(props) => {
+    props.as === "h2" &&
+      css`
+        font-size: 2rem;
+        font-weight: 600;
+      `;
+  }}
+  ${test}
+`;
 
-export default Heading
+export default Heading;
