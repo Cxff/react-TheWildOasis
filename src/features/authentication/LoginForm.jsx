415d8ed68f2775f471977a080608fe5271951c5c
Cxff <adidaodididoudou@163.com>
2024-08-20 11:37:56 +0800

    feat: 项目打包部署到Netlify服务器上

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("adidaodididoudou@gmail.com");
+  const [password, setPassword] = useState("a123456");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
